53bc516e6b8e0e9624d801cfea850759dfd4d3cd
mardurango <mlcardona85@mmmisena.edu.co>
2019-08-10 20:08:38 -0500

    cambios en migration y configuracion de redis

--- database/migrations/2019_08_11_003705_crear_tabla_menu_rol.php
+++ database/migrations/2019_08_11_003705_crear_tabla_menu_rol.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CrearTablaPermisoRol extends Migration
+class CrearTablaMenuRol extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,11 @@ class CrearTablaPermisoRol extends Migration
      */
     public function up()
     {
-        Schema::create('permiso_rol', function (Blueprint $table) {
-            $table->bigIncrements('id');
+        Schema::create('menu_rol', function (Blueprint $table) {
             $table->bigInteger('rol_id')->unsigned();
             $table->foreign('rol_id')->references('id')->on('rol');
-            $table->bigInteger('permiso_id')->unsigned();
-            $table->foreign('permiso_id')->references('id')->on('permiso');
+            $table->bigInteger('menu_id')->unsigned();
+            $table->foreign('menu_id')->references('id')->on('menu');
             $table->charset = 'utf8mb4';
             $table->collation = 'utf8mb4_spanish_ci';
         });
@@ -31,6 +30,6 @@ class CrearTablaPermisoRol extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('permiso_rol');
+        Schema::dropIfExists('menu_rol');
     }
 }
